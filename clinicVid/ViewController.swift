a707740ef18e1e943cf6ba87d545fc8a9eb330de
Kyle Chadwick <kylepchad@gmail.com>
2018-01-29 06:04:40 -0800

    vertical plane detection

--- clinicVid/ViewController.swift
+++ clinicVid/ViewController.swift
@@ -13,82 +13,67 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
-    
      let configuration = ARWorldTrackingConfiguration()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //sceneView.autoenablesDefaultLighting = true
-        sceneView.session.run(configuration)
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // turn on vertical plane detection
+        configuration.planeDetection = .vertical
         
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        // run configuration
         sceneView.session.run(configuration)
+
+        sceneView.delegate = self
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return}
+        let wallArtNode = createWallArt(planeAnchor: planeAnchor)
+        node.addChildNode(wallArtNode)
+        print("vertical surface detected")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return}
+        print("updating wall's anchor...")
+        node.enumerateChildNodes{(childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        let wallArtNode = createWallArt(planeAnchor: planeAnchor)
+        node.addChildNode(wallArtNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
         
-        // Pause the view's session
-        sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    func createWallArt(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        //let wallArtNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
+        let wallArtNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        wallArtNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "wallArt")
+        wallArtNode.geometry?.firstMaterial?.isDoubleSided = true
+        //wallArtNode.position = SCNVector3(0,0,-1)
+        wallArtNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        wallArtNode.eulerAngles = SCNVector3(3.14/2, 0,0)
+        print("art added")
+        return wallArtNode
     }
 
+    func createArt() -> SCNNode {
+        let ArtNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
+        ArtNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "wallArt")
+        ArtNode.position = SCNVector3(0,0,-1)
+        print("art added")
+        return ArtNode
+    }
+    
     @IBAction func mask(_ sender: Any) {
-        let node = SCNNode()
-        node.geometry = SCNPlane(width: 100, height: 100)
-        node.position = SCNVector3(0, 0, -100)
-        self.sceneView.scene.rootNode.addChildNode(node)
-    }
-    
-    
-    
-    
-    
-    
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        // add masking plane to background
     }
 }
